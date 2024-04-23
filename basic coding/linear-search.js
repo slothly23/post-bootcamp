f348e28960c2677710f59a8951616fb7fe33f7a5
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-04-23 15:39:49 +0700

    linear search using buit-in function

--- basic coding/linear-search.js
+++ basic coding/linear-search.js
@@ -1,12 +1,12 @@
-// use For
 let arr1 = [8 ,3 ,9 ,2 ,5],
     arr2 = [2 ,6 ,7 ,4 ,1],
     arr3 = [2 ,6 ,7 ,5 ,1]
 
+// use For
 function findNumber (arr, number) {
     for (let i = 0; i < arr.length; i++ ) {
         if ( arr[i] === number ) {
-            return number
+            return arr[i]
         }
     }
 }
@@ -14,3 +14,14 @@ function findNumber (arr, number) {
 console.log(findNumber(arr1, 3));
 console.log(findNumber(arr2, 4));
 console.log(findNumber(arr3, 9));
+
+
+// use built-in function
+function findNum (arr, number) {
+   return arr.find( element => element === number )
+}
+
+console.log('===============');
+console.log( findNum(arr1, 3));
+console.log( findNum(arr2, 4));
+console.log( findNum(arr3, 9));
