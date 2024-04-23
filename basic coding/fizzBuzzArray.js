a72ec42af8d1e11cb73d4248ffedb6711b056c7f
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-04-23 22:34:05 +0700

    FizzBuzzArray using for loop

---
+++ basic coding/fizzBuzzArray.js
@@ -0,0 +1,35 @@
+let arr1 = [8 ,3 ,9 ,2 ,5],
+    arr2 = [3 ,4 ,6 ,7 ,9],
+    arr3 = [4 ,5 ,9 ,15 ,19],
+    arr4 = [2 ,4 ,7 ,8 ,1]
+
+// ketentuan:
+// habis dibagi 3 dan tidak habis dibagi 5 -> 'fizz'
+// habis dibagi 5 dan tidak habis dibagi 3 -> 'buzz'
+// habis dibagi 3 dan 5 -> 'fizz buzz'
+// lainnya -> elemen array
+
+// use for 
+function fizzBuzzArray (arr) {
+    
+    for ( let i = 0; i < arr.length; i++ ) {
+        if ( arr[i] % 3 === 0 && arr[i] % 5 !== 0 ) {
+            arr[i] = 'fizz'
+        } 
+        else if ( arr[i] % 5 === 0 && arr[i] % 3 !== 0 ) {
+            arr[i] = 'buzz'
+        }
+        else if ( arr[i] % 3 === 0 && arr[i] % 5 === 0 ) {
+            arr[i] = 'fizz' + ' ' + 'buzz'
+        } else {
+            arr[i]
+        }
+    }
+    
+    return arr
+}
+
+console.log(fizzBuzzArray(arr1)); // [ 8, 'fizz', 'fizz', 2, 'buzz' ]
+console.log(fizzBuzzArray(arr2)); // [ 'fizz', 4, 'fizz', 7, 'fizz' ]
+console.log(fizzBuzzArray(arr3)); // [ 4, 'buzz', 'fizz', 'fizz buzz', 19 ]
+console.log(fizzBuzzArray(arr4)); // [ 2, 4, 7, 8, 1 ]
